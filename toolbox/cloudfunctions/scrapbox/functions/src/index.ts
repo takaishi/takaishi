9f66cfddc2e7c6f1c2fde3e730e8531c58a38075
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-08-31 19:02:26 +0900

    add function for scrapbox

---
+++ toolbox/cloudfunctions/scrapbox/functions/src/index.ts
@@ -0,0 +1,64 @@
+import * as functions from "firebase-functions";
+import * as dayjs from "dayjs";
+import "dayjs/locale/ja"; // これimportしないとエラー吐かれる
+import fetch from "node-fetch";
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const index = functions.region('asia-northeast1').https.onRequest(async (request, response) => {
+  const d = request.query.date
+  console.log(`d: ${d}`)
+  if (d == undefined || d == '') {
+    const today = new Date();
+    const yearMonthDay = dayjs(today).locale("ja").format("YYYY-MM-DD");
+    response.send(await run(today, yearMonthDay));
+  } else {
+    const today = new Date();
+    response.send(await run(today, d.toString()));
+  }
+});
+
+async function run(today: Date, yearMonthDay: string) {
+  const status = await getPage(yearMonthDay);
+
+  if (status == 200) {
+    return {exist: true, body: `https://scrapbox.io/rtakaishi/${yearMonthDay}`};
+  } else {
+    const prevTitle = dayjs(new Date().setDate(today.getDate() - 1)).format("YYYY-MM-DD");
+    const nextTitle = dayjs(new Date().setDate(today.getDate() + 1)).format("YYYY-MM-DD");
+
+    const year = dayjs(today).locale("ja").format("YYYY");
+    const yearMonth = dayjs(today).locale("ja").format("YYYY-MM");
+    const monthDay = dayjs(today).locale("ja").format("MM-DD");
+
+    const num = new Array(new Date().getFullYear() - 2020).fill(null).map((_, i) => i + 1);
+    const nYearsAgo = num.map(function(n) {
+      const today = new Date();
+      return `[${dayjs(today.setFullYear(today.getFullYear() - n)).format("YYYY-MM-DD")}]`;
+    }).join("\n");
+    const body = `
+[*** 今日の n 年前]
+
+${nYearsAgo}
+
+[*** ナビ]
+
+前日 [${prevTitle}]
+翌日 [${nextTitle}]
+
+[Journal]  [${yearMonthDay}]  [${year}]  [${yearMonth}]  [${monthDay}]
+`;
+    return {exist: false, body: `https://scrapbox.io/rtakaishi/${yearMonthDay}?body=${encodeURI(body)}`};
+  }
+}
+
+async function getPage(date: string) {
+  const resp = await fetch(`https://scrapbox.io/rtakaishi/${date}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  return resp.status;
+}
